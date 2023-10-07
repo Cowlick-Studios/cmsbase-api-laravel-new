012b2fc80b913f02686f34934d87f83399242cb1
Jack Woodrow <jackwoodrow99@gmail.com>
2023-10-07 15:57:41 -0400

    Add daily request count to dashboard

--- app/Http/Controllers/tenant/DashboardController.php
+++ app/Http/Controllers/tenant/DashboardController.php
@@ -11,6 +11,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Symfony\Component\Finder\Finder;
 
+use App\Models\tenant\RequestLog;
+use App\Models\tenant\Setting;
+
 class DashboardController extends Controller
 {
 
@@ -33,19 +36,29 @@ class DashboardController extends Controller
 
   public function index(Request $request){
     try {
-      $schemaName = "tenant-" . tenant()->id;
 
+      // Storage Limits
+      $schemaName = "tenant-" . tenant()->id;
       $query = DB::select("
         SELECT SUM(pg_total_relation_size(quote_ident(tablename)::text)) AS schema_size
         FROM pg_tables
         WHERE schemaname = '{$schemaName}'
-      "); // bytes
+      ");
+
+      // Requests
+      $requestCount = null;
+
+      $logRequestSetting = Setting::where('key', 'request_logging')->first();
+      if($logRequestSetting->value == 'true'){
+        $requestCount = RequestLog::where('created_at', '>=', now()->subHours(24))->count();
+      }
 
       return response([
         'message' => 'Dashboard.',
         'tenant' => tenant(),
         'database_usage' => (int) $query[0]->schema_size,
-        'file_usage' => $this->getTotalSizeOfFilesInDirectory(storage_path())
+        'file_usage' => $this->getTotalSizeOfFilesInDirectory(storage_path()),
+        'daily_request_count' => $requestCount
       ], 200);
     } catch (Exception $e) {
       return response([
