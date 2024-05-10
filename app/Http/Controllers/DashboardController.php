f18df8c9db0f6f7c75db1708b934218dafac79e8
Jack Woodrow <jackwoodrow99@gmail.com>
2024-05-10 00:54:12 -0400

    add total number of tenants to admin dash

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\VerifyEmail;
 use Carbon\Carbon;
 use App\Http\Controllers\Controller;
+use App\Models\Tenant;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Symfony\Component\Finder\Finder;
@@ -34,10 +35,24 @@ class DashboardController extends Controller
       $totalDiskSpace = disk_total_space('/');
       $freeDiskSpace = disk_free_space('/');
 
+      $totalDeligatedFile = 0;
+      $totalDeligatedDatabase = 0;
+
+      $allTenants = Tenant::all();
+
+      foreach($allTenants as $tenant){
+        $totalDeligatedFile += $tenant->storage_limit_file;
+        $totalDeligatedDatabase += $tenant->storage_limit_database;
+      }
+
+
       return response([
         'message' => 'Dashboard.',
         'total_disk_space' => ceil($totalDiskSpace / 1048576),
-        'free_disk_space' => ceil($freeDiskSpace / 1048576)
+        'free_disk_space' => ceil($freeDiskSpace / 1048576),
+        'deligated_database' => $totalDeligatedDatabase * 1024,
+        'deligated_file' => $totalDeligatedFile * 1024,
+        'tenant_count' =>$allTenants->count()
       ], 200);
     } catch (Exception $e) {
       return response([
